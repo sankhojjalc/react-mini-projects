c4b62079c486546aa3fe9603f129aaff753a1c94
sankhojjal <sankhojjalc@gmail.com>
2024-03-10 23:16:55 +0530

    implemented country search

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 // import { Accordian } from "./components/accordian";
 // import { RandomColorGenerator } from "./components/randomColorGenerator/";
 // import { Timer } from "./components/timer";
-import { StarRating } from "./components/star-rating";
+// import { StarRating } from "./components/star-rating";
+import { SearchAndDebounce } from "./components/search-country-debounce";
 
 export const App = () => (
   <>
     {/* <Accordian /> */}
     {/* <RandomColorGenerator /> */}
     {/* <Timer /> */}
-    <StarRating />
+    {/* <StarRating /> */}
+    <SearchAndDebounce />
   </>
 );
---
+++ src/components/search-country-debounce/index.jsx
@@ -0,0 +1,83 @@
+/**
+ * Implement a dynamic search of countries- DONE
+ * Implement debounce
+ * Render the capital of the country when user clicks search button -  DONE
+ */
+
+import { useState, useEffect } from "react";
+import style from "./index.module.css";
+
+export const SearchAndDebounce = () => {
+  const [countryList, setCountryList] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState(null);
+  const [input, setInput] = useState("");
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const fetchCountryList = async () => {
+    const list_raw = await fetch(
+      `https://restcountries.com/v3.1/name/${input}`
+    );
+    const list = await list_raw.json();
+    const listOfFive = list?.slice(0, 5);
+    const filterCountryName = listOfFive
+      ?.map((item) => item.name.common)
+      .filter((name) => name);
+
+    setCountryList(filterCountryName);
+  };
+
+  const handleSelectCountry = async (country) => {
+    const country_raw = await fetch(
+      `https://restcountries.com/v3.1/name/${country.target.innerText}?fullText=true`
+    );
+    const countrySelected = await country_raw.json();
+
+    setSelectedCountry(countrySelected);
+  };
+
+  useEffect(() => {
+    input.length > 2 && fetchCountryList();
+    if (input.length === 0) {
+      setCountryList([]);
+    }
+  }, [input]);
+
+  return (
+    <div className={style.wrapper}>
+      <div className={style.searchWrapper}>
+        <div className={style.searchWrapperChild}>
+          <input
+            placeholder="Search Country..."
+            type="text"
+            value={input}
+            onChange={handleInputChange}
+          />
+          {countryList.length > 0 && (
+            <div className={style.search}>
+              <ul>
+                {countryList?.map((list) => (
+                  <li key={list} onClick={(e) => handleSelectCountry(e)}>
+                    {list}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
+        {selectedCountry?.length > 0 && (
+          <>
+            <h3 className={style.capital}>
+              Selected Country: {selectedCountry?.[0]?.name?.common}
+            </h3>
+            <h3 className={style.capital}>
+              Capital: {selectedCountry?.[0]?.capital?.[0]}
+            </h3>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
