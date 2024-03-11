e22a204fec3b57761cfe1b2affb56d5c68d62eff
sankhojjal <sankhojjalc@gmail.com>
2024-03-11 23:31:09 +0530

    adding the first question

---
+++ src/components/kbc-quiz-game/data.js
@@ -0,0 +1,8 @@
+export const data = [
+  {
+    key: 1,
+    question: "What is the capital of India",
+    options: ["New Delhi", "Mumbai", "Kolkata", "Chennai"],
+    answer: "New Delhi",
+  },
+];
