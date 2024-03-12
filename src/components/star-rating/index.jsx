d4491309fb31a5b6a16f2ed6a377d853ddab78ea
sankhojjal <sankhojjalc@gmail.com>
2024-03-13 00:45:52 +0530

    implemented logic for hovering in star rating app

--- src/components/star-rating/index.jsx
+++ src/components/star-rating/index.jsx
@@ -1,30 +1,44 @@
+/* eslint-disable react/prop-types */
 import { useState } from "react";
 
 const stars = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const Star = ({ setSelectedStar, starId }) => (
+const Star = ({ setSelectedStar, starId, setHoveredStar }) => (
   <i
     className="bi bi-star"
     style={{ fontSize: "2rem" }}
+    onMouseOver={() => setHoveredStar(starId)}
     onClick={() => setSelectedStar(starId)}
   ></i>
 );
-const FilledStar = ({ setSelectedStar, starId }) => (
+const FilledStar = ({ setSelectedStar, starId, setHoveredStar }) => (
   <i
     className="bi bi-star-fill"
     style={{ fontSize: "2rem" }}
     onClick={() => setSelectedStar(starId)}
+    onMouseOver={() => setHoveredStar(starId)}
   ></i>
 );
 
 export const StarRating = () => {
   const [selectedStar, setSelectedStar] = useState(0);
+  const [hoveredStar, setHoveredStar] = useState(0);
 
   return stars.map((star) => {
-    return star <= selectedStar ? (
-      <FilledStar setSelectedStar={setSelectedStar} starId={star} key={star} />
+    return star <= (selectedStar || hoveredStar) ? (
+      <FilledStar
+        setSelectedStar={setSelectedStar}
+        starId={star}
+        key={star}
+        setHoveredStar={setHoveredStar}
+      />
     ) : (
-      <Star key={star} setSelectedStar={setSelectedStar} starId={star} />
+      <Star
+        key={star}
+        setSelectedStar={setSelectedStar}
+        starId={star}
+        setHoveredStar={setHoveredStar}
+      />
     );
   });
 };
