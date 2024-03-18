2087bb93b961bef28c4189c7dafe9f25afd24798
sankhojjal <sankhojjalc@gmail.com>
2024-03-19 01:13:56 +0530

    added data for folder structure

---
+++ src/components/folderStructure/data.js
@@ -0,0 +1,140 @@
+export const data = {
+  children: [
+    {
+      name: "Khetradas",
+      key: "1",
+      children: [
+        {
+          name: "Bhabani Charan",
+          key: "1-1",
+          children: [
+            {
+              name: "Ujjal",
+              key: "1-1-1",
+              children: [
+                {
+                  name: "Sankhojjal",
+                  key: "1-1-1-1",
+                  children: [],
+                },
+              ],
+            },
+            {
+              name: "Kajol",
+              key: "1-1-2",
+            },
+            {
+              name: "Shyamal",
+              key: "1-1-3",
+              children: [
+                {
+                  name: "Sawan",
+                  key: "1-1-3-1",
+                  children: [],
+                },
+              ],
+            },
+            {
+              name: "Biman",
+              key: "1-1-4",
+              children: [
+                {
+                  name: "Bonoshree",
+                  key: "1-1-1-4",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          name: "Birendranath",
+          key: "1-2",
+          children: [
+            {
+              name: "Priya",
+              key: "1-2-1",
+            },
+            {
+              name: "Madhab",
+              key: "1-2-2",
+              children: [
+                {
+                  name: "Debam",
+                  key: "1-2-2-1",
+                  children: [],
+                },
+                {
+                  name: "Shubham",
+                  key: "1-2-2-2",
+                  children: [],
+                },
+              ],
+            },
+          ],
+        },
+        {
+          name: "Rathendranath",
+          key: "1-3",
+          children: [
+            {
+              name: "Chaitali",
+              key: "1-3-1",
+            },
+            {
+              name: "Rajib",
+              key: "1-3-2",
+              children: [
+                {
+                  name: "Rohan",
+                  key: "1-3-2-1",
+                  children: [],
+                },
+              ],
+            },
+            {
+              name: "Rajat",
+              key: "1-3-3",
+            },
+          ],
+        },
+        {
+          name: "Ramendranath",
+          key: "1-4",
+        },
+        {
+          name: "Nabakumar",
+          key: "1-5",
+          children: [
+            {
+              name: "Sajal",
+              key: "1-5-1",
+              children: [
+                {
+                  name: "X",
+                  key: "1-5-1-1",
+                  children: [],
+                },
+              ],
+            },
+            {
+              name: "Sanjeeb",
+              key: "1-5-2",
+              children: [
+                {
+                  name: "Raja",
+                  key: "1-5-2-1",
+                  children: [],
+                },
+                {
+                  name: "Goja",
+                  key: "1-5-2-2",
+                  children: [],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
