a0aaed0216e3784917c4e348e9f2581f387d2463
sankhojjal <sankhojjalc@gmail.com>
2024-09-16 00:42:38 +0530

    removed wrong tag

--- src/components/BrowserPagination/selectNumberOfItems.jsx
+++ src/components/BrowserPagination/selectNumberOfItems.jsx
@@ -3,7 +3,7 @@ import { usePagination } from "./usePagination";
 export const SelectNumberOfItemsPerPage = () => {
   const { setUsersPerPage } = usePagination();
   return (
-    <select onChange={() => setUsersPerPage(event.target.value)} selec>
+    <select onChange={() => setUsersPerPage(event.target.value)}>
       <option value={10}>10</option>
       <option value={20} selected>
         20
