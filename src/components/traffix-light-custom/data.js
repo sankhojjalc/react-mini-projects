7747f3d3b9293243d92f697b0b202bcfbc0624e4
sankhojjal <sankhojjalc@gmail.com>
2024-09-28 00:55:37 +0530

    completed custom traffic light

--- src/components/traffix-light-custom/data.js
+++ src/components/traffix-light-custom/data.js
@@ -1,9 +1,19 @@
-export const RED = "red";
-export const YELLOW = "yellow";
-export const GREEN = "green";
+const RED = "red";
+const YELLOW = "yellow";
+const GREEN = "green";
 
 export const data = [
   { color: RED, timer: 1000 },
   { color: YELLOW, timer: 2000 },
   { color: GREEN, timer: 5000 },
 ];
+
+export const debounce = (fn, delay = 1000) => {
+  let timer;
+  return (...args) => {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn(...args);
+    }, delay);
+  };
+};
--- src/components/traffix-light-custom/index.jsx
+++ src/components/traffix-light-custom/index.jsx
@@ -1,32 +1,44 @@
 import { useState, useEffect } from "react";
 import { Light } from "./Light";
-import { data } from "./data";
+import { data, debounce } from "./data";
 
 export const TrafficLightCustom = () => {
   const [count, setCount] = useState(0);
   const [presentColorCtx, setPresentColorCtx] = useState(data[0]);
+  const [goGreenFlag, setGoGreenFlag] = useState(false);
+
   let timeCtx = null;
 
   //this will set the signal as green and timer will stop for green.
-  const handleGoGreen = () => {};
-  //this will increase the timer of the present signal by 1000ms- handle with debounce
-  const handleIncreaseTimer = () => {};
+  const handleGoGreen = () => {
+    clearTimeout(timeCtx);
+    setGoGreenFlag(true);
+    setPresentColorCtx(data[2]);
+  };
+  //this will increase the timer of the present signal by 3000ms- handle with debounce
+  const handleIncreaseTimer = () => {
+    setPresentColorCtx((prev) => ({ ...prev, timer: prev.timer + 3000 }));
+  };
   //this will reset the signal
-  const handleReset = () => {};
-
+  const handleReset = () => {
+    setGoGreenFlag(false);
+  };
+  const debounceFn = debounce(handleIncreaseTimer);
   useEffect(() => {
-    timeCtx = setTimeout(() => {
-      if (count < 3) {
-        setCount((prev) => prev + 1);
-        setPresentColorCtx((prev) => ({ ...prev, ...data[count] }));
-      } else {
-        setCount(0);
-        setPresentColorCtx((prev) => ({ ...prev, ...data[0] }));
-      }
-    }, presentColorCtx.timer);
+    if (!goGreenFlag) {
+      timeCtx = setTimeout(() => {
+        if (count < 3) {
+          setCount((prev) => prev + 1);
+          setPresentColorCtx((prev) => ({ ...prev, ...data[count] }));
+        } else {
+          setCount(0);
+          setPresentColorCtx((prev) => ({ ...prev, ...data[0] }));
+        }
+      }, presentColorCtx.timer);
+    }
 
     return () => clearTimeout(timeCtx);
-  }, [count, presentColorCtx]);
+  }, [count, presentColorCtx, goGreenFlag]);
 
   return (
     <>
@@ -45,7 +57,7 @@ export const TrafficLightCustom = () => {
         ))}
       </div>
       <button onClick={handleGoGreen}>Go Green</button>
-      <button onClick={handleIncreaseTimer}>Increase Timer</button>
+      <button onClick={debounceFn}>Increase Timer</button>
       <button onClick={handleReset}>Reset</button>
     </>
   );
